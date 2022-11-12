512e3b68bfec63dc76407ef552dc703188d59d50
unknown <samuelnimi11@gmail.com>
2022-11-12 17:41:20 +0200

    changed text typing animation text

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //js typing animation
 var typed = new Typed(".typing", {
-    strings: ["", 'World traveler', 'Football fan', 'Web devloper', '', 'Good kid'],
+    strings: ["", 'Football fan', 'Web devloper', '', 'Good kid', 'World traveler'],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
