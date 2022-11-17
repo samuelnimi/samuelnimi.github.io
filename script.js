7225e1f9390ca3f0877bc817b600e6fd188e9306
unknown <samuelnimi11@gmail.com>
2022-11-17 16:06:50 +0200

    Removed some websites from work

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ var typed = new Typed(".typing", {
     strings: ["", 'Football fan', 'Web devloper', 'Good kid', 'Trumpet learner'],
     typeSpeed: 100,
     backSpeed: 60,
-    loop: true
+    
 })
